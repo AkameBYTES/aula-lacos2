b072870573ead0d4b054b78685ae7ccf36ea90ed
アカメ <141885614+AkameBYTES@users.noreply.github.com>
2023-09-04 21:54:56 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+/*const comparar = (num1, num2) => {
+    if (num1 > num2) {
+        console.log("É maior") ;
+    } else  (num1 < num2) {
+        console.log("Menor");
+    } else if {
+        console.log("Iguais")
+    }
+}
+const num1 = prompt("Digite um número")
+const num2 = prompt("Digite outro número")
+*/
+/*
+const escolhido = prompt("Escolha um Pokemon").toLowerCase();
+switch (escolhido) {
+    case 'Bulbassauro': 
+console.log("planta e veneno");
+break;
+case 'Charmander':
+console.log("Fogo");
+break
+case 'Squirtle':
+    console.log("Àgua");
+    break;
+    default:
+        console.log("Pokemon não identificado")
+        break
+}
+*/
+/*
+const podeEstudar = (formadoEm, idade, estudando) => {
+if(formadoEm === true && idade ) {
+
+}
+}
+*/
