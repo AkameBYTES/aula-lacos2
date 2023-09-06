464432b506913c749f80c21160a87435fcff066b
アカメ <141885614+AkameBYTES@users.noreply.github.com>
2023-09-05 21:50:35 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,36 +1,108 @@
-/*const comparar = (num1, num2) => {
-    if (num1 > num2) {
-        console.log("É maior") ;
-    } else  (num1 < num2) {
-        console.log("Menor");
-    } else if {
-        console.log("Iguais")
-    }
+//Exercícios de Laços
+/*Exercício de interpretação de códigos
+Exercício 1
+let valor = 0
+for(let i = 0; i < 5; i++) {
+  valor += i
 }
-const num1 = prompt("Digite um número")
-const num2 = prompt("Digite outro número")
-*/
-/*
-const escolhido = prompt("Escolha um Pokemon").toLowerCase();
-switch (escolhido) {
-    case 'Bulbassauro': 
-console.log("planta e veneno");
-break;
-case 'Charmander':
-console.log("Fogo");
-break
-case 'Squirtle':
-    console.log("Àgua");
-    break;
-    default:
-        console.log("Pokemon não identificado")
-        break
-}
-*/
-/*
-const podeEstudar = (formadoEm, idade, estudando) => {
-if(formadoEm === true && idade ) {
+console.log(valor)
+ 10
+ */
 
+ //Exercício 2
+ //1(1)a.)
+ /*const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+for (let numero of lista) {
+  if (numero > 18) {
+		console.log(numero)
+	}
 }
+19
+21
+23
+25
+27
+30
+*/
+//2(1)b.)
+//Não
+
+//Exercício 3
+//3(1)a.)
+//3
+//3(1)b.)
+
+//Exercícios de Escrita de código
+//Exercício 1
+//1(2)a.)
+/*
+for(let i = 0; i < 2; i++) {
+  const animal = prompt("Quantos animais você possui?");
+  if(animal < i) {
+    console.log("Você necessita de companhia, adote um.")
+  } else(animal > i); {
+    console.log("Otimo, você é feliz.")
+    const nomeAnimais = ["Mistral", "Armstrong"];
+    function devolverNomes(nomeAnimais) {
+      let todos = 0
+      for(let i = 0; i< nomeAnimais.lenght; i++) {
+        let animalatual = nomeAnimais[i]
+        if (animalatual >= todos) {
+          todos = animalatual
+        }
+      }
+    }}
 }
+*/
+//Exerciocio 2
+// Este array será usado para exemplificar as respostas de todos os itens
+/*const array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+
+// Resposta item a.
+80
+30
+130
+40
+60
+21
+70
+120
+90
+103
+110
+55
+
+// Resposta item b.
+8
+3 
+13
+4
+6
+2.1
+7 
+12
+9
+10.3
+11 
+5.5
+
+// Resposta item c.
+[80, 30, 130, 40, 60, 70, 120, 90, 110] 
+
+// Resposta item d.
+[ 'O elemento do índex 0 é 80',
+  'O elemento do índex 1 é 30',
+  'O elemento do índex 2 é 130',
+  'O elemento do índex 3 é 40',
+  'O elemento do índex 4 é 60',
+  'O elemento do índex 5 é 21',
+  'O elemento do índex 6 é 70',
+  'O elemento do índex 7 é 120',
+  'O elemento do índex 8 é 90',
+  'O elemento do índex 9 é 103',
+  'O elemento do índex 10 é 110',
+  'O elemento do índex 11 é 55' ]
+
+// Resposta e.
+"O maior número é 130 e o menor é 21"
 */
